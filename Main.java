3274dc296d01495a35714393ffc6c758e1aed4d4
longwatermelon <longwatermelon1@gmail.com>
2022-10-26 21:43:08 -0700

    .

--- Main.java
+++ Main.java
@@ -25,21 +25,17 @@ public class Main {
         int[] frac = new int[3];
 
         int i = s.indexOf("_");
-        if (i != -1)
-        {
+        if (i != -1) {
             frac[0] = Integer.parseInt(s.substring(0, i));
             s = s.substring(i + 1, s.length());
         }
 
         i = s.indexOf("/");
 
-        if (i != -1)
-        {
+        if (i != -1) {
             frac[1] = Integer.parseInt(s.substring(0, i));
             frac[2] = Integer.parseInt(s.substring(i + 1, s.length()));
-        }
-        else
-        {
+        } else {
             frac[1] = Integer.parseInt(s);
             frac[2] = 1;
         }
@@ -50,8 +46,7 @@ public class Main {
     static int[] calculate(int[] f1, int[] f2, char op) {
         int[] res = new int[2];
 
-        if (op == '+' || op == '-')
-        {
+        if (op == '+' || op == '-') {
             f1[1] *= f2[2];
             f2[1] *= f1[2];
 
@@ -64,14 +59,12 @@ public class Main {
             else           res[0] = f1[1] - f2[1];
         }
 
-        if (op == '*')
-        {
+        if (op == '*') {
             res[0] = f1[1] * f2[1];
             res[1] = f1[2] * f2[2];
         }
 
-        if (op == '/')
-        {
+        if (op == '/') {
             res[0] = f1[1] * f2[2];
             res[1] = f1[2] * f2[1];
         }
@@ -97,17 +90,13 @@ public class Main {
         res[1] = Math.abs(res[1]);
         res[0] = Math.abs(res[0] % res[1]);
 
-        if (res[0] % res[1] == 0 && whole == 0)
-        {
+        if (res[0] % res[1] == 0 && whole == 0) {
             System.out.println("0");
-        }
-        else
-        {
-            if (whole != 0) System.out.print(whole);
-            else System.out.print(sign > 0 ? "" : "-");
+        } else {
+            if (whole != 0)  System.out.print(whole);
+            else             System.out.print(sign > 0 ? "" : "-");
 
-            if (res[0] % res[1] != 0 && whole != 0)
-            {
+            if (res[0] % res[1] != 0 && whole != 0) {
                 System.out.print("_");
                 System.out.print(res[0] % res[1]);
                 System.out.print("/");
